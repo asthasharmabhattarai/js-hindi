5d49dffcfbb692841e6ec65b450d63e15fecce5e
asthasharmabhattarai <55975200+asthasharmabhattarai@users.noreply.github.com>
2025-07-28 14:09:20 +0545

    Create 02-datatypes.js

---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,20 @@
+/* there are many datatypes
+1. number -> 2 to power 53
+2. string -> "" 
+3. bigint -> big numbers
+4. boolean -> true/false
+5. null -> standalone 
+6. undefined -> no value
+*/
+
+let studentPhone = 12345678
+let studentName = "astha"
+let startedCourse = true
+let bigPhone = 9828372873287382738273827
+let value1=12.34
+let value2= null
+let value3
+console.log(typeof studentPhone)
+console.table([typeof studentPhone, typeof studentName, typeof startedCourse, typeof bigPhone, typeof value1, typeof value2, typeof value3])
+//startedCourse, bigPhone, value1, value2 )
+
