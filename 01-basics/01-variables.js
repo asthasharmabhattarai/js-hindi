89a437d934b46dc0cac82a7c16ee79da3a7d1b35
asthasharmabhattarai <55975200+asthasharmabhattarai@users.noreply.github.com>
2025-07-28 14:08:52 +0545

    Create 01-variables.js

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,16 @@
+const customerId = 11
+var customerName = "Lochan"
+let customerCity ="Ktm"
+var customerState ="bagmati"
+
+
+if(0 == 0)
+    {
+        let customerState = "she said \"hi\""
+        var customerName = "Kanchi"
+        console.log(customerState)
+        console.log(customerName)
+    }
+
+console.table([customerId,customerName,customerCity,customerState])
+//..dont use var to declare variable as it will replace all values of same variablename , not suggested
