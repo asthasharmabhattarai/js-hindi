45748bad0585852be854b16b9ba646b175434bea
asthasharmabhattarai <55975200+asthasharmabhattarai@users.noreply.github.com>
2025-07-28 14:10:17 +0545

    Create 03-conversions.js

---
+++ 01-basics/03-conversions.js
@@ -0,0 +1,16 @@
+let phone = 123
+
+let stringPhone = String(phone) 
+let bigIntPhone = BigInt(phone)
+let booleanPhone = Boolean(phone)
+
+console.log(stringPhone)
+console.log(typeof stringPhone)
+console.log(bigIntPhone)
+console.log(typeof bigIntPhone)
+console.log(booleanPhone)
+console.log(typeof booleanPhone)
+
+// 123 -> string -> "123"
+// 123 -> bigInt -> 123n
+// 123 -> boolean -> true
